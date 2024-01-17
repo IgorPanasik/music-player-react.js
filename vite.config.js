9e6922970affabe053b5627c368ec662bb00c7a8
IgorPanasik <panasik.igor.com@gmail.com>
2024-01-17 20:04:02 +0300

    Set vite config base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+	base: '/music-player-react.js/',
 	plugins: [react()],
 });
