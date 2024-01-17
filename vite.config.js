297f5adeea226b4a42e1c4102802bb6be498ef37
IgorPanasik <panasik.igor.com@gmail.com>
2024-01-17 22:36:45 +0300

    fix directory dist

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
+import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
+	base: '/music-player-react.js/',
 	plugins: [
+		react(),
 		viteStaticCopy({
 			targets: [
 				{ src: 'src/assets/sounds/*', dest: 'assets/sounds' },
